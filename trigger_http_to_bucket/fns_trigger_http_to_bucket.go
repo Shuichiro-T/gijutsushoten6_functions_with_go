85dc927c0773fbcaae20e1a2fa44741c984f1e06
Shuichiro.TAKAHASHI <reg@shuichiro.jp>
2019-02-24 17:00:42 +0900

    Make Write Bucket.

--- trigger_http_to_bucket/fns_trigger_http_to_bucket.go
+++ trigger_http_to_bucket/fns_trigger_http_to_bucket.go
@@ -24,45 +24,46 @@ func TriggerHTTPToBucket(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		//Storageへ出力する関数を呼び出す
-		MakeFile(w, names[0])
+		//6.Storageへ出力する関数を呼び出す
+		WriteBucket(w, names[0])
 
-	default: //6.それ以外の場合はエラー
+	default: //7.それ以外の場合はエラー
 		http.Error(w, "405 - Method Not Allowed", http.StatusMethodNotAllowed)
 	}
 }
 
-func MakeFile(rw http.ResponseWriter, name string) {
+//8.Storageへ出力する関数を定義する
+func WriteBucket(rw http.ResponseWriter, name string) {
 
-	//7.環境変数からバケット名を取得する
+	//9.環境変数からバケット名を取得する
 	bucketName := os.Getenv("BUCKET_NAME")
 
-	//8.Storageへ接続するクライアントを初期化する
+	//10.Storageへ接続するクライアントを初期化する
 	ctx := context.Background()
 	client, err := storage.NewClient(ctx)
 
-	//9.エラーの場合は処理を終了する
+	//11.エラーの場合は処理を終了する
 	if err != nil {
 		fmt.Fprint(rw, "Storage接続エラー　エラータイプ：%T、エラーメッセージ：%s", err, err)
 		return
 
 	}
-	//10.接続したクライアントは確実に切断させる
+	//12.接続したクライアントは確実に切断させる
 	defer client.Close()
 
-	//11.オブジェクト名を決める yyyyMMddhhmmss形式
+	//13.オブジェクト名を決める yyyyMMddhhmmss形式
 	objectName := time.Now().Format("20060102150405")
 
-	//12.オブジェクトを作成して書き込みストリームを開く
+	//14.オブジェクトを作成して書き込みストリームを開く
 	fw := client.Bucket(bucketName).Object(objectName).NewWriter(ctx)
 
-	//13.ファイルを書き込み　エラーの場合は処理を終了する
+	//15.ファイルを書き込み　エラーの場合は処理を終了する
 	if _, err := fw.Write([]byte(name + "\r\n")); err != nil {
 		fmt.Fprint(rw, "オブジェクト書き込みエラー　エラータイプ：%T、エラーメッセージ：%s", err, err)
 		return
 	}
 
-	//14.書き込みストリームを閉じる、エラーの場合は処理を終了する
+	//16.書き込みストリームを閉じる、エラーの場合は処理を終了する
 	if err := fw.Close(); err != nil {
 		fmt.Fprint(rw, "オブジェクト切断エラー　エラータイプ：%T、エラーメッセージ：%s", err, err)
 		return
